230af6c039e8128d642057c2b7bc594af511d43c
bszczerba13 <bartekk29alk@gmail.com>
2026-05-05 10:40:25 +0200

    Filtering test added

---
+++ src/test/java/com/practicesoftwaretesting/tests/FilteringTest.java
@@ -0,0 +1,20 @@
+package com.practicesoftwaretesting.tests;
+
+import com.practicesoftwaretesting.base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class FilteringTest extends BaseTest {
+
+    @Test
+    public void filteringByCategoryTest(){
+        String category = "Hammer";
+        homePage.filterByCategory(category);
+        List<String> productTitles = homePage.getProductTitles();
+        for (String title : productTitles){
+            Assert.assertTrue(title.contains(category.toLowerCase()));
+        }
+    }
+}
