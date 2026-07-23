4393c5ab8f77c8c0fba9ec7ad65c5d66b36c5f15
bszczerba13 <bartekk29alk@gmail.com>
2026-07-23 21:12:55 +0200

    Add multi-browser support

--- src/test/java/com/practicesoftwaretesting/base/DriverFactory.java
+++ src/test/java/com/practicesoftwaretesting/base/DriverFactory.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 /**
  * Creates and manages WebDriver instance.
@@ -14,25 +17,61 @@ public class DriverFactory {
     private static WebDriver driver;
 
     /**
-     * Initializes browser instance based on provided browser name.
+     * Initializes a WebDriver instance based on the configured browser.
      *
-     * @return WebDriver instance
+     * @return initialized WebDriver instance
+     * @throws IllegalArgumentException if the configured browser is not supported
      */
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
         String browser = TestConfiguration.getBrowser();
-        if ("firefox".equalsIgnoreCase(browser)) {
-            driver = new FirefoxDriver();
-        }
-        else {
+        boolean headless = TestConfiguration.isHeadless();
+
+        if ("chrome".equalsIgnoreCase(browser)) {
             ChromeOptions options = new ChromeOptions();
-            if (TestConfiguration.isHeadless()) {
+
+            if (headless) {
                 options.addArguments("--headless=new");
                 options.addArguments("--window-size=1920,1080");
             } else {
                 options.addArguments("start-maximized");
             }
+
             driver = new ChromeDriver(options);
+
+        } else if ("firefox".equalsIgnoreCase(browser)) {
+            FirefoxOptions options = new FirefoxOptions();
+
+            if (headless) {
+                options.addArguments("--headless");
+                options.addArguments("--width=1920");
+                options.addArguments("--height=1080");
+            }
+
+            driver = new FirefoxDriver(options);
+
+            if (!headless) {
+                driver.manage().window().maximize();
+            }
+
+        } else if ("edge".equalsIgnoreCase(browser)) {
+            EdgeOptions options = new EdgeOptions();
+
+            if (headless) {
+                options.addArguments("--headless=new");
+                options.addArguments("--window-size=1920,1080");
+            }
+
+            driver = new EdgeDriver(options);
+
+            if (!headless) {
+                driver.manage().window().maximize();
+            }
+        } else {
+            throw new IllegalArgumentException(
+                    "Unsupported browser: " + browser + ". Supported browsers: chrome, firefox, edge."
+            );
         }
+
         return driver;
     }
 
